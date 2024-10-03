e122c55033e2b77423da8d1d280ac0b7bc97ad43
Syed zainul abedin <szsimplesyed93@gmail.com>
2024-10-03 22:45:32 +0530

    Added command invoker

---
+++ Assets/Scripts/Command/CommandInvoker.cs
@@ -0,0 +1,16 @@
+public class CommandInvoker {
+    private Stack<ICommand> commandHistory = new Stack<ICommand>();
+
+    public void ProcessCommand(ICommand command) {
+        ExecuteCommand(command);
+        RegisterCommand(command);
+    }
+
+    private void ExecuteCommand(ICommand command) {
+        command.Execute();
+    }
+
+    private void RegisterCommand(ICommand command) {
+        commandHistory.Push(command);
+    }
+}
