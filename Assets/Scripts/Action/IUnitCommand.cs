8e5510d3615e6c476ece9eee0acc1dc63f201347
Syed zainul abedin <szsimplesyed93@gmail.com>
2024-10-03 22:42:35 +0530

    Added unit command

---
+++ Assets/Scripts/Action/IUnitCommand.cs
@@ -0,0 +1,13 @@
+public class IUnitCommand : ICommand
+{
+    public int ActorUnitId { get; set; }
+    public int TargetUnitId { get; set; }
+    public int ActorPlayerId { get; set; }
+    public int TargetPlayerId { get; set; }
+
+    protected UnitController actor;
+    protected UnitController target;
+
+    public abstract void Execute();
+    public abstract bool WillHitTarget();
+}
